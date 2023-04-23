e77bf0fce17c085c496ae690e8d0eaad5cafeb93
Lucas <lucassoares2007@hotmail.com>
2023-04-23 17:28:11 -0300

    build: base project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  next();
+});
+
+export default app;
